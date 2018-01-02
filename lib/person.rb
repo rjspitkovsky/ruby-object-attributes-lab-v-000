993ec47d7d0360f5ac8c39aced0ee44fb938fe2d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-02 21:29:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
-class Person 
+class Person
   def name=(new_name)
-    @name = new_name 
-  end 
-  def name 
-    @name 
-  end 
+    @name = new_name
+  end
+  def name
+    @name
+  end
   def job=(new_job)
-    @job = new_job 
+    @job = new_job
+  end
+  def job
+    @job
   end 
-  def job 
-    @job 
-  end 
-end 
+end
